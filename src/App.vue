70b7f6eaea028eb03b9313aead7f9726b7826935
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-18 15:55:50 +0200

    add component Header, ComponentPage

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
-<script setup>
+<script>
+import Header from './components/Header.vue';
+import ComponentPage from './components/ContentPage.vue'
+
+export default {
+  
+  components: {
+  Header,
+  ComponentPage,
+}
+}
 </script>
 
 <template>
-  
+  <Header></Header>
+  <ComponentPage></ComponentPage>
 </template>
 
 <style scoped>
