1caadec8fba0c28f7cb0e09c7ade8d35a86b063c
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-18 16:07:57 +0200

    add component for content page

---
+++ src/components/ContentPage.vue
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<template >
+    <div>
+    --&gt; Content goes here &lt;--
+    </div>
+</template>
+
+<style scoped>
+    div {
+        background-color: #000;
+        color: #fff;
+        font-size: 2em;
+        padding-block: 2em;   
+    }
+</style>
