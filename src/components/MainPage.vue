c65c59ab62c2ea5a869c2b005daa167c7e81bd13
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-19 22:46:19 +0200

    add file

---
+++ src/components/MainPage.vue
@@ -0,0 +1,36 @@
+<script lang="js">
+import Jumbo from './Jumbo.vue';
+import CurrentSeries from './CurrentSeries.vue';
+import comicsJSON from '../json/dc-comics.json';
+
+export default {
+    data() {
+        return {
+        comicsJSON: comicsJSON,
+        }
+    },
+
+    components: {
+        Jumbo,
+        // CurrentSeries,
+    }
+}
+</script>
+
+<template>
+    <Jumbo />
+    <div>
+        <CurrentSeries v-for="cardComics in comicsJSON"
+        :thumb="cardComics.thumb"
+        :price="cardComics.price"
+        :series="cardComics.series"
+        :type="cardComics.type"
+        />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    div {
+        background-color: #1C1C1C;
+    }
+</style>
