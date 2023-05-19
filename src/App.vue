0de9039037191fa238df665015e56ddec0dff46a
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-19 22:52:22 +0200

    change button in "a" in style/ change ComponentPage in MainPage

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,35 @@
 <script>
 import Header from './components/Header.vue';
-import ContentPage from './components/ContentPage.vue'
+// import MainPage from './components/MainPage.vue';
 import LinkBar from './components/LinkBar.vue';
 import FooterPage from './components/FooterPage.vue';
 
 export default {
-  
+
   components: {
-  Header,
-  ContentPage,
-  LinkBar,
-  FooterPage,
-}
+    Header,
+    // MainPage,
+    LinkBar,
+    FooterPage,
+  }
 }
 </script>
 
 <template>
   <Header></Header>
-  <ContentPage />
+  <!-- <MainPage /> -->
   <LinkBar />
   <FooterPage />
 </template>
 
 <style lang="scss">
-* {
-  margin: 0;
-  padding: 0,
-}
-button {
-        background-color: rgba(0, 0, 0, 0);
-        border: none;
-}
+  * {
+    margin: 0;
+    padding: 0;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+  a {
+    text-decoration: none;
+    color: #000;
+  }
 </style>
