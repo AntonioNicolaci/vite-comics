a9ba37ee9da73ec49e91396d27619f238a5134e4
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-19 22:50:49 +0200

    add file

---
+++ src/components/CurrentSeries.vue
@@ -0,0 +1,22 @@
+<script lang="js">
+export default {
+    
+}
+
+defineProps({
+        thumb: String, // link immagine
+        price: String, // prezzo
+        series: String, // serie del fumetto
+        type: String, // tipo di fumetto
+    })
+</script>
+
+<template>
+    <div>
+        <img :src="thumb" :alt="series">
+    </div>
+</template>
+
+<style lang="" scoped>
+    
+</style>
