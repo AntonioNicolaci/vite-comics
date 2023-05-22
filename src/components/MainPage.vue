7942ccc975738cc6d337dc0f0966bf704369d054
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-22 14:11:17 +0200

    add style/ add tag/ change object array name

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,4 +1,4 @@
-<script lang="js">
+<script>
 import Jumbo from './Jumbo.vue';
 import CurrentSeries from './CurrentSeries.vue';
 import comicsJSON from '../json/dc-comics.json';
@@ -6,31 +6,64 @@ import comicsJSON from '../json/dc-comics.json';
 export default {
     data() {
         return {
-        comicsJSON: comicsJSON,
+            comic: comicsJSON,
         }
     },
 
     components: {
         Jumbo,
-        // CurrentSeries,
+        CurrentSeries,
     }
 }
 </script>
 
 <template>
-    <Jumbo />
-    <div>
-        <CurrentSeries v-for="cardComics in comicsJSON"
-        :thumb="cardComics.thumb"
-        :price="cardComics.price"
-        :series="cardComics.series"
-        :type="cardComics.type"
-        />
-    </div>
+    <main>
+        <Jumbo />
+        <div id="tagMid">CURRENT SERIES</div>
+        <div id="divCard">
+            <CurrentSeries v-for="cardComics in comic"
+            :thumb="cardComics.thumb"
+            :price="cardComics.price"
+            :series="cardComics.series"
+            :type="cardComics.type"
+            />
+        </div>
+        <a href="#">LOAD MORE</a>
+    </main>
+    
 </template>
 
 <style lang="scss" scoped>
-    div {
+@import 'style.scss';
+    main {
+        position: relative;
         background-color: #1C1C1C;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    #tagMid {
+        position: absolute;
+        z-index: 1;
+        padding: 1em;
+        background-color: $primary-color;
+        color: #fff;
+        top: calc(540px - 1.5em);
+        left: 0;
+    }
+    #divCard {
+        display: flex;
+        flex-wrap: wrap;
+        padding: 3em 1em 2em 1em;
+        gap: 1em;
+        row-gap: 2em;
+    }
+    a {
+        background-color: $primary-color;
+        padding: 1em 4em;
+        font-weight: bold;
+        color: #fff;
+        margin-block: 2em;
     }
 </style>
